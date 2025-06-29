e6fe9eb1760f4ffb5ddfc279ae29f851c6065e89
Abhishek Mathur <abhishekmathurofficial@gmail.com>
2025-06-29 22:59:44 +0530

    edited constants

--- frontend/src/components/ui/SearchBar.jsx
+++ frontend/src/components/ui/SearchBar.jsx
@@ -34,7 +34,7 @@ function SearchBar() {
                     </div>
 
                     <p className='text-xs sm:text-sm md:text-base text-base-content text-center'>
-                        Not signed in? <Link to="/auth/sign-up"><span className='underline underline-offset-2'>Create</span></Link> an account to get started!
+                        Not signed in? <Link to="/auth/sign-up"><span><div className="tooltip"><div className="tooltip-content"> <div className="animate-bounce text-orange-400 -rotate-10 text-2xl font-black">Step Out!</div></div><span className='underline underline-offset-2'>Create</span></div></span></Link> an account to get started!
                     </p>
                 </div>
 
